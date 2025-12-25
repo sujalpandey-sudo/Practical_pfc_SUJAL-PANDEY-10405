6e2fd84fc9b5a109bc6c3595a3f5b4330fcf5109
sujalpandey-sudo <sujal.pandey@rungta.org>
2025-12-25 16:45:01 +0530

    Create main.c Practical 6C
    Create main.c Practical 6C

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    for (int i = a; i <= b; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
